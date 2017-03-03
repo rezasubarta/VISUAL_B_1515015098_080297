d4032698a53bc4acc1388cbc43d00bd46797cc5e
rezasubarta <rezasubarta@gmail.com>
2017-03-03 15:18:36 +0800

    Tugas 1 Selesai

---
+++ PVPosttest1/src/pvposttest1/FrameTugas1_MainClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pvposttest1;
+
+/**
+ *
+ * @author A455L
+ */
+public class FrameTugas1_MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrameTugas1 reza = new FrameTugas1();
+        reza.setVisible(true);
+    }
+}
+        
+            
+            
+                
+            
+
